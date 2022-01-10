8b763b839b586fa1d18d8e915a776a3eaa4cc8a5
mobile44 <albertwong.wk@gmail.com>
2022-01-09 23:04:51 -0800

    add: added responsive

--- src/App.js
+++ src/App.js
@@ -30,7 +30,8 @@ const App=()=>{
   return(
     <BrowserRouter>
       <nav>
-        {(toggleMenu || screenWidth > 500) && (
+        {/*{(toggleMenu || screenWidth > 800) && (*/}
+        {(screenWidth > 800) && (
           <div className="topNav">
             <div className="topMenu">
               <a href='https://www.facebook.com/KEV-Group-111378062266170/'>
@@ -76,10 +77,36 @@ const App=()=>{
           </div>
         )}
         {/*<button onClick={toggleNav} className="btn">Menu</button>*/}
-        <div className="shortMenu" onClick={toggleNav}>
-          <div className="bar1"></div>
-          <div className="bar2"></div>
-          <div className="bar3"></div>
+        <div className="minNav">
+          <div className="mediaBar">
+            <a href='https://www.facebook.com/KEV-Group-111378062266170/'>
+              <img src={facebook} alt='facebook' width="30" height="30"/>
+            </a>
+            <a href='https://www.linkedin.com/company/kev-group/'>
+              <img src={linkedin} alt='linkedin' width="30" height="30"/>
+            </a>
+            <a href='https://twitter.com/kevgroup?lang=en'>
+              <img src={twitter} alt='twitter' width="30" height="30"/>
+            </a>
+            <a href='https://www.instagram.com/kevgroup/'>
+              <img src={instagram} alt='instagram' width="30" height="30"/>
+            </a>
+            <div className="shortMenu" onClick={toggleNav}>
+              <div className="bar1"></div>
+              <div className="bar2"></div>
+              <div className="bar3"></div>
+            </div>
+          </div>
+          {(toggleMenu) && (
+            <div className="firstBar">
+              <Link className="link" to="/react_jsx_test/" >Blog</Link>
+              <Link className="link" to="/react_jsx_test/" >Careers</Link>
+              <Link className="link" to="/react_jsx_test/" >In the News</Link>
+              <Link className="link" to="/react_jsx_test/" >Order Forms</Link>
+              <Link className="link" to="/react_jsx_test/" >Contact</Link>
+          </div>
+          )}
+          
         </div>
       </nav>
       
