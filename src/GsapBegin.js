73a508de88215c16001abb860c1d2e4b94ba74cb
mobile44 <albertwong.wk@gmail.com>
2022-01-16 15:26:23 -0800

    fix: forwardRef

--- src/GsapBegin.js
+++ src/GsapBegin.js
@@ -12,38 +12,41 @@ function Container({children}) {
 
 const Sqr = forwardRef(({children}, ref)=>{
   return <div className="box3" ref={ref}>{children}</div>;
-})
+});
 
 function Divider({children}) {
   return <div><Sqr>No Bother</Sqr></div>;
 }
 
-function Cube({children}) {
+function Cube1({children}) {
   return <div className="box4">{children}</div>
 }
 
 function Circle1({children}) {
-  return <div className="circle1">{children}</div>
+  return <div className="circle1"><div className="circletxt1">{children}</div></div>
 }
 
 function GsapBegin() {
   const el1 = useRef();
   const el2 = useRef();
+  const el3 = useRef();
   const sqr1 = useRef();
   const sqr2 = useRef();
+  const tl1 = useRef();
   const [reversed, setReversed] = useState(false);
 
-  const q = gsap.utils.selector(el2);
+  const q1 = gsap.utils.selector(el2);
+  const q2 = gsap.utils.selector(el3);
 
   useEffect(() => {
     gsap.to(el1.current, { rotation: "+=360" });
-
-    gsap.to(q(".box2"),{
+    
+    gsap.to(q1(".box2"),{
       x: 100,
       stagger: 0.33,
       repeat: -1,
       repeatDelay: 1,
-      duration: 3,
+      duration: 4,
       yoyo: true,
     });
 
@@ -53,22 +56,44 @@ function GsapBegin() {
       x: 150,
       repeat: -1,
       repeatDelay: 1,
-      duration: 3,
+      duration: 4,
       yoyo: true,
-    })
-  });
+    });
+
+    tl1.current = gsap.timeline()
+      .to(q2(".box4"), {
+        rotate: 360,
+      })
+      .to(q2(".circle1"), {
+        x: 100
+      });
     
+  },[]);
+  
+  useEffect(()=>{
+    tl1.current.reversed(reversed);
+  },[reversed]);
+  
   return (
     <div className="gsapBody">
       <div className="gsapPage">
         <div className="box1" ref={el1}>Hello</div>
         <div ref={el2}>
-        <Container ref={el2}/>
-        <Box>GSAP</Box>
+          <Container/>
+          <Box>GSAP</Box>
         </div>
-        <Sqr ref={sqr1}>Square 1</Sqr>
-        <Divider></Divider>
-        <Sqr ref={sqr2}>Square 2</Sqr>
+        <div>
+          <Sqr ref={sqr1}>Square 1</Sqr>
+          <Divider></Divider>
+          <Sqr ref={sqr2}>Square 2</Sqr>
+        </div>
+        <div className="cube" ref={el3}>
+          <div>
+            <button onClick={()=>setReversed(!reversed)}>Toggle</button>
+          </div>
+          <Cube1>cube</Cube1>
+          <Circle1>circle</Circle1>
+        </div>
       </div>
     </div>
   ) 
