019fa2cb7ced04a0c6b3329a4a4ed86f54ac55fe
mobile44 <albertwong.wk@gmail.com>
2022-01-20 18:40:51 -0800

    optimize: random page

--- src/Random.js
+++ src/Random.js
@@ -1,6 +1,6 @@
 import React, {useRef, useEffect, useState} from "react";
 import { gsap } from "gsap";
-import "./GsapBegin.css";
+import "./Random.css";
 
 const randomX = gsap.utils.random(-100, 100, 1, true);
 
@@ -10,6 +10,7 @@ function Rect({children, endX}) {
     gsap.to(rectRef.current, {
       x: endX,
       duration: 4,
+      ease: "bounce",
     });
   },[endX])
   return <div className="box6" ref={rectRef}>{children}</div>;
@@ -18,10 +19,10 @@ function Rect({children, endX}) {
 function Random() {
   const [endX, setEndX] = useState(0);
   return (
-    <div className="gsapBody">
-      <div className="gsapPage">
-        <div className="randomPage">
-          <button onClick={()=>setEndX(randomX())}>Radmom Now</button>
+    <div className="randomBody">
+      <div className="randomPage">
+        <div className="randomSection">
+          <button className="randomToggle" onClick={()=>setEndX(randomX())}>Radmom Move</button>
           <Rect endX={endX}>{endX}</Rect>
         </div>
       </div>
